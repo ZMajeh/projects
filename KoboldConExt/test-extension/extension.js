7fbcf7ecdcc9783d8a8cedff67a8e38f0360d997
majeh <zmajeh@gmail.com>
2026-03-27 18:50:22 +0530

    adding icon to koboldcpp extension

---
+++ KoboldConExt/test-extension/extension.js
@@ -0,0 +1,10 @@
+function activate(context) {
+  console.log('Congratulations, your extension "test-extension" is now active!');
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
